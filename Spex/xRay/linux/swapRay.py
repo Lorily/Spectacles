bf5650feda86ed80dc396941d50d19dab6abafb0
Scott Rogers <trash80@trash80.org>
2013-02-04 22:07:32 -0600

    A few updates, but mostly some files to get ready to pull the intended information.

--- Spex/xRay/linux/swapRay.py
+++ Spex/xRay/linux/swapRay.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-
+#TODO: Shouldn't this be in with HDD?
 from .baseRay import BaseRay
 
 class SwapRay(BaseRay):
     """
-
+    Get the system hdd swap size.
     """
 
     def __init__(self):
